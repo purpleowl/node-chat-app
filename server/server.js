b89686c12952d4fc4c91b55f12bad7cb2c31a36e
purpleowl <endo@en.local>
2016-11-26 08:34:33 +0900

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const publickPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publickPath));
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+})
+
